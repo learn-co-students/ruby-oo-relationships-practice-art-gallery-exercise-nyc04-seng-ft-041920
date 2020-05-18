4ddac94010fb9c0c992402cb66e4cb4f1c818302
senadakadric <skadric@fordham.edu>
2020-05-18 11:30:31 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,20 @@
 require_relative '../config/environment.rb'
 
 
+# to test my code
+
+ar1 = Artist.new("Senada", 23)
+ar2 = Artist.new("Mimi", 18)
+ar3 = Artist.new("Bee", 1)
+
+g1 = Gallery.new("Guggenheim", "NYC")
+g2 = Gallery.new("The Met", "Chicago")
+
+#def initialize(title, price, artist, gallery)
+p1 = Painting.new("La Bonita", 1000000, ar1, g1)
+p2 = Painting.new("Lalala", 10, ar2, g1)
+p3 = Painting.new("PolandSpring", 233548694939, ar3, g2)
+p4 = Painting.new("Bob Ross Portrait", 500, ar1, g2)
 
 
 binding.pry
