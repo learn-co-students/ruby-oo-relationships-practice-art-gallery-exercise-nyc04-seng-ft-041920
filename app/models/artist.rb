1ae7cf2a789f05b9749fe52e3e6450be98cd3a8c
github username <github email address>
2020-05-18 11:27:20 -0400

    submitting code challenge

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,53 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+  end
+
+  def galleries
+    # find all the artist's paintings
+    # find all the galleries
+    self.paintings.map{|painting| painting.gallery}.uniq #adding uniq to remove duplicates
+  end
+
+  def cities
+    self.galleries.map{|gallery| gallery.city}
+  end
+
+  def self.total_experience
+    self.all.reduce(0){|total, artist| total + artist.years_experience}
+  end
+
+  def self.most_prolific
+    most_pro_artist = nil
+    most_pro_artist_value = 0
+
+    self.all.each do |artist|
+      prolific_value = artist.paintings.size / artist.years_experience.to_f
+      if prolific_value > most_pro_artist_value
+        most_pro_artist = artist
+        most_pro_artist_value = prolific_value
+      end
+    end
+    most_pro_artist
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,29 @@
 require_relative '../config/environment.rb'
 
 
+a1 = Artist.new("Monet", 45)
+a2 = Artist.new("Michelangelo", 28)
+a3 = Artist.new("Bob Ross", 50)
+a4 = Artist.new("Elisheva", 1)
+
+
+g1 = Gallery.new("MOMA", "NYC")
+g2 = Gallery.new("Tel Aviv Art Gallery", "Tel Aviv")
+g3 = Gallery.new("Elisheva's Gallery", "Highland Park")
+
+p1 = Painting.new("Water Lilies", 9999, a1, g3)
+p2 = Painting.new("Pretty Mountains", 500, a3, g1)
+p3 = Painting.new("Sistene Chapel", 12345, a2, g2)
+p4 = Painting.new("Lake and Trees", 475, a3, g2)
+p5 = Painting.new("Ocean", 625, a3, g1)
+# p6 = Painting.new("My painting", 100, a4, g1)
+# p7 = Painting.new("Black dot on white canvas", 100000, a4, g1)
+
+
+
+
+
+
 
 
 binding.pry
